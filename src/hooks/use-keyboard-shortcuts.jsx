a1f77dd340064b7cdf5290102afc647250200d37
Gaurav Thakur <gthakur581@gmail.com>
2024-09-12 00:45:36 +0530

    fix issue with prevent default

--- src/hooks/use-keyboard-shortcuts.jsx
+++ src/hooks/use-keyboard-shortcuts.jsx
@@ -3,15 +3,18 @@ import { useEffect } from "react";
 export function useKeyboardShortcuts(setOpen) {
   useEffect(() => {
     const handleChange = (event) => {
-      event.preventDefault();
       if (event.ctrlKey && event.key === "o") {
+        event.preventDefault();
         setOpen(true);
       }
       if (event.metaKey && event.key === "o") {
+        event.preventDefault();
         setOpen(true);
       }
     };
+
     document.body.addEventListener("keydown", handleChange);
+
     return () => {
       document.body.removeEventListener("keydown", handleChange);
     };
